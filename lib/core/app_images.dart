98e73cc71b23198277bd19359662d88c140f075a
Abnner <abnner.sales@inventsoftware.com.br>
2021-04-21 13:38:58 -0300

    Primeira Aula da NLW Flutter

---
+++ lib/core/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static String get hierarchy => "assets/hierarchy.png";
+  static String get data => "assets/data.png";
+  static String get laptop => "assets/laptop.png";
+  static String get blocks => "assets/blocks.png";
+  static String get check => "assets/check.png";
+  static String get error => "assets/error.png";
+  static String get trophy => "assets/trophy.png";
+  static String get logo => "assets/logo.png";
+}
